f4aa252c1fe76fdb5a8d40eaedc38f9f66a9a00d
Akshit Agarwal <akshit@email.arizona.edu>
2024-11-20 02:00:46 -0600

    eda until Search_Promotions

--- food.py
+++ food.py
@@ -25,15 +25,37 @@ def perform_eda(train_df, test_df):
     print(set(test_df['Facility_Category'].values))
     print('=====================================================================')
 
-    print('check zip code info..')
+    print('now checking ---->zip code info..')
     print((set(train_df['City_Zip_Code'].values)))
     print((set(test_df['City_Zip_Code'].values)))
     print('There are 8 distinct zipcodes. Need to replace 0 with a dummy zipcode, 123')
     print('=====================================================================')
 
-    print('now checking Operational_Region_Coverage_Area')
+    print('now checking ----> Operational_Region_Coverage_Area')
     print(train_df.isnull().sum())
     print(train_df.isin([0]).sum())
+    print(len(set(train_df['Operational_Region_Coverage_Area'].values)))
+    print(len(set(test_df['Operational_Region_Coverage_Area'].values)))
+    print('There are 28 distinct values in Operational_Region_Coverage_Area --> no nulls, zeros found')
+    print('=====================================================================')
+
+    print('now checking ----> Custom Promoted')
+    print((set(train_df['Custom_Promoted'].values)))
+    print((set(test_df['Custom_Promoted'].values)))
+    print("This is a categorical column with 0s and 1s")
+    print('=====================================================================')
+
+    print('now checking ---->Promoted')
+    print((set(train_df['Promoted'].values)))
+    print((set(test_df['Promoted'].values)))
+    print("This is a categorical column with 0s and 1s")
+    print('=====================================================================')
+
+    print('now checking ----> Custom Promoted')
+    print((set(train_df['Search_Promotions'].values)))
+    print((set(test_df['Search_Promotions'].values)))
+    print("This is a categorical column with 0s and 1s")
+    print('=====================================================================')
 
     return
 
